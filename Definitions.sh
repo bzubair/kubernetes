b92e122fd3ce5c9f386de7f04635cdcfa19eef8d
Zubair <bilal.zubair.ext@on.nokia.com>
2016-07-07 14:08:18 -0400

    Adding files

---
+++ Definitions.sh
@@ -0,0 +1,82 @@
+*****Kubernetes Concepts*****
+
+So put it all together 
+	"Containers" runs on clusters 
+		"PODs" are containers that work together
+			"Services" are PODS that work together like a web farm
+				"Labels" are used to organize services
+https://www.youtube.com/watch?v=qCxYjq7EBHc
+
+NODE: 
+	run Docker, kubelet and proxy
+	Prepare a Node Manifest
+
+POD
+	Pods are a colocated group of application containers with shared volumes
+	Share some namespaces like network namespace
+	It runs a infrastructure deamon to grap the ip from Docker
+	Each POD has one IP NOT every container
+	Containers in a POD share the namespaces includng their IP addresses
+	Pods can be created individually, but it is recommended that you use a replication controller even if creating a single pod.
+	A pod generally represents one or more containers that should be controlled as a single "application".
+
+API SERVERS 
+	API Servers are the "ONLY SERVERS which talks to ETCD"
+	This is the one which understand all the "MANIFESTS"
+	is the main management point of the entire cluster. 
+	A client called "kubecfg" is packaged along with the server-side tools and can be used from a local computer or by connecting to the master server.
+
+SCHEDULER
+	Best fit choses based on POD requirement
+	Only schedules job to nodes which a re healthy
+	Scheduler tackes action --> picks a nodes in a cluster --> Puts a POD in its namespace in etcd and new pod will be launched
+	Unhealthy nodes will be removed
+	Pluggable Other schedulers can used like mesos
+
+REPLICATION CONTROLLERS
+	Replication controllers manage the lifecycle of pods.
+	Creates PODS from templates (POD Manifest)	
+	Ensures desired number of PODS are running
+	Self-healing.. If pods goes down --reschedule it somewhere else
+
+SERVICE
+	Services provide a single, stable name and address for a set of pods. 
+	They act as basic load balancers.
+	Can have VIP per service 
+
+ETCD 
+	This is where all the configuration is stored
+	If ETCD is gone then everything is gone so cluster ETCD servers are recommended
+	Kubernetes uses etcd to store configuration data that can be used by each of the nodes in the cluster. 
+
+FLANNELD
+	This is for overlay network 
+
+CADVISOR
+	Kubelet has this tool built in which works with scheduler to give output of the resources used or free. This will help SCHEDULER choose node and deploy PODS there
+	CADVISOR gets it from CGROUPS
+
+
+PROXY SERVICE
+	Portal_net configuration isused to define the VIP subnet for services
+
+CLOUD PROVIDER
+	Kubernetes has the concept of a Cloud Provider, which is a module which provides an interface for managing TCP Load Balancers, Nodes (Instances) and Networking Routes.
+
+KUBERNETES NETWORKING 
+	Highly-coupled container-to-container communications: this is solved by pods and localhost communications
+	Pod-to-Service communications: this is covered by services.
+	External-to-Service communications: this is covered by services
+	Pod-to-Pod communications: 
+			all containers can communicate with all other containers without NAT
+			all nodes can communicate with all containers (and vice-versa) without NAT
+			the IP that a container sees itself as is the same IP that others see it as
+
+KUBEKET SERVICE 
+	is the main contact point for each minion with the cluster group.
+	This service is responsible for relaying information to and from the master server, as well as interacting with the etcd store to read configuration details or write new values
+
+
+
+
+
---
+++ Kubernetes Notes.sh
@@ -0,0 +1,239 @@
+
+******CoreOS*********
+- At boot, CoreOS reads a user-supplied configuration file called "cloud-config" to do some initial configuration. 
+- Usually, the "cloud-config" file will, at a minimum, tell the host how to join an existing cluster and command the host to boot up two services called etcd and fleet. configuration
+- The etcd daemon is used to store and distribute data to each of the hosts in a cluster.
+	This is useful for keeping consistent configurations and it also serves as a platform with which services can announce themselves.
+- The fleet daemon is basically a distributed init system.
+	It handles service scheduling, constraining the deployment targets based on user-defined criteria.
+
+
+Container related info:
+- cgroups: are often in /sys/fs/cgroups. Used for resource metering and limiting
+- Rocket is like docker
+
+Tools:
+- cAdvisors: it takes the resource number and give it a times series. If a vm is resouce is using 100% resource right now then what it was using before
+
+*****Kubernetes*******
+
+
+
+	
+	
+	
+	
+	
+
+
+				
+				
+	- "Services" is a unit that acts as a basic load balancer and ambassador for other containers.
+				Services are an interface to a group of containers so that consumers do not have to worry about anything beyond a single access location
+				* A set of POD working together i.e putting LB frontend and BE can be used under one service
+	- "Replication Controllers"
+				A replication controller is a framework for defining pods that are meant to be horizontally scaled.
+				This means that if a container temporarily goes down, the replication controller might start up another container.
+				If the first container comes back online, the controller will kill off one of the containers.				 
+	- "Labels"	is basically an arbitrary tag that can be placed on the above work units to mark them as a part of a group.
+				These can then be selected for management purposes and action targeting
+				Each unit can have more than one label, but each unit can only have one entry for each key
+
+
+Kubernetes has several components:
+
+etcd - A highly available key-value store for shared configuration and service discovery.
+flannel - An etcd backed network fabric for containers.
+kube-apiserver - Provides the API for Kubernetes orchestration.
+kube-controller-manager - Enforces Kubernetes services.
+kube-scheduler - Schedules containers on hosts.
+kubelet - Processes a container manifest so the containers are launched according to how they are described.
+kube-proxy - Provides network proxy services.
+
+
+****Kubernetes Installation - Centos7****
+http://severalnines.com/blog/installing-kubernetes-cluster-minions-centos7-manage-pods-services
+https://www.youtube.com/watch?v=tA8XNVPZM2w
+1. systemctl stop/disable firewalld
+2. yum -y install ntp, systemctl start/enable/sync ntpd
+
+
+	**Kube Master Installation**
+	1. Install Centos 7.1
+	2. yum -y install etcd kubernetes
+	3. edit /etc/etcd/etcd.conf
+		ETCD_NAME=default
+		ETCD_DATA_DIR="/var/lib/etcd/default.etcd"
+		ETCD_LISTEN_CLIENT_URLS="http://<kubemaster-ip>:2379"
+		ETCD_ADVERTISE_CLIENT_URLS="http://localhost:2379"
+	4. Edit /etc/kubernetes/apiserver
+		KUBE_API_ADDRESS="--address=0.0.0.0"
+		KUBE_API_PORT="--port=8080"
+		KUBELET_PORT="--kubelet_port=10250"
+		KUBE_ETCD_SERVERS="--etcd_servers=http://127.0.0.1:2379"
+		KUBE_SERVICE_ADDRESSES="--service-cluster-ip-range=10.254.0.0/16"
+		KUBE_ADMISSION_CONTROL="--admission_control=NamespaceLifecycle,NamespaceExists,LimitRanger,SecurityContextDeny,ResourceQuota"
+		KUBE_API_ARGS=""
+	5. Start and enable etcd kube-apiserver kube-controller-manager kube-scheduler
+	
+	6. Define flannel network configuration in etcd. 
+		etcdctl mk /atomic.io/network/config '{"Network":"172.17.0.0/16"}'
+		**In this scenario we are using atomic.ip but have multiple other options like coreos etc
+
+	7. Check to see if any Miniosn nodes are added	
+		kubectl get nodes
+
+	**Kubernetes Minions Installation**
+	1. Install Centos 7.1 with physical drive 20 and another volume group (vdb) atleast 10
+		- create volume and attach to vm
+		- "pvcreate /dev/vdb" (Attached/create second disk to vm)
+		- "pvs" (check the second disk)
+		- "vgextend centos /dev/vdb" (Add disk to volume group named centos)
+		- "vgs" to check the additional disk space is added to volume group
+		- "vgdisplay -v | more" "pvdisplay" "lvdisplay" other command to very space
+	2. yum -y install flannel kubernetes
+	3. Edit /etc/sysconfig/flanneld to configure etcd server 
+		FLANNEL_ETCD="http://192.168.0.134:2379"
+		FLANNEL_ETCD_KEY="/atomic.io/network"
+		#FLANNEL_ETCD_KEY="/coreos.com/network"
+	4. Edit  /etc/kubernetes/config to define kubernet server
+		KUBE_MASTER="--master=http://192.168.0.134:8080"
+	5. Edit /etc/kubernetes/kubelet to configure kubelet service
+		KUBELET_ADDRESS="--address=0.0.0.0"
+		KUBELET_PORT="--port=10250"
+		# change the hostname to this host’s IP address
+		KUBELET_HOSTNAME="--hostname_override=minion1.lab.local"
+		KUBELET_API_SERVER="--api_servers=http://kubemaster.lab.local:8080"
+		KUBELET_ARGS=""
+	6. Start and enable kube-proxy, kubelet, docker and flanneld services
+	7. Login to Kubemaster and check if node is added "kubectl get nodes"
+
+
+*************************
+	  CLUSTER CONFIG
+*************************
+1. NON-SECURE cluster setup (????)
+kubectl config set-credentials myself --username=admin --password=secret
+kubectl config set-cluster local-server --server=http://localhost:8080
+kubectl config set-context default-context --cluster=local-server --user=myself
+kubectl config use-context default-context
+kubectl config set contexts.default-context.namespace the-right-prefix
+kubectl config view
+
+
+*************************
+	  PROXY SETTING
+*************************
+
+On Minions following cnfiguration required
+1. mkdir /etc/systemd/system/docker.service.d
+2. Edit "/etc/systemd/system/docker.service.d/http-proxy.conf " the enter the following configure
+		[Service]
+		Environment="HTTP_PROXY=http://proxy.lbs.alcatel-lucent.com:8000/"
+		Environment="HTTPS_PROXY=https://proxy.lbs.alcatel-lucent.com:8000/"
+3. For no-proxy (OPTIONAL)
+		Environment="HTTP_PROXY=http://proxy.example.com:80/" "NO_PROXY=localhost,127.0.0.1,docker-registry.somecorporation.com"
+4. systemctl daemon-reload
+5. systemctl restart docker
+
+NOTE: MAY BE you need to go to minion and tun "docker images" if you see gcr.io/google_containers/pause then delete this docker image 
+	docker  -D rmi -f 2c40b0526b63
+
+*************************
+	  CREATING PODS
+*************************
+	--> Create yaml file (checj sample yaml is kubernetes folder)
+	--> auto-create the API token for  service Account (http://stackoverflow.com/questions/31891734/not-able-to-create-pod-in-kubernetes)
+			- openssl genrsa -out /tmp/serviceaccount.key 2048
+			- vi /etc/kubernetes/apiserver:
+					KUBE_API_ARGS="--service_account_key_file=/tmp/serviceaccount.key"
+			- vi /etc/kubernetes/controller-manager
+					KUBE_CONTROLLER_MANAGER_ARGS="--service_account_private_key_file=/tmp/serviceaccount.key"
+			- systemctl restart kube-controller-manager.service
+	--> kubectl create -f pod.yaml
+	--> kubectl get pods
+			kubectl get events --watch
+			kubectl logs <POD-NAME>
+			kubectl cluster-info
+
+
+Image deployment
+kubectl run my-nginx --image=nginx --replicas=2 --port=80 --expose --service-overrides='{ "spec": { "type": "LoadBalancer" } }'
+
+***************************
+	"TROUBLESHOOTING"
+***************************
+	Check events "bin/kubectl -s x.x.x.x:8080 get events --namespace=kube-system"
+	To check more commands "https://github.com/kubernetes/kubernetes/issues/11684"
+	kubectl get events --watch
+	if docker throw error message about expection 172.16.54.1 or something like then 
+			run "systemctl daemon-reload"
+			reboot machine
+
+****Kubernetes commands****
+--> "NODES RELATED"
+	kubectl get nodes -- to check online nodes
+	kubectl describe nodes
+
+--> "Deleting PODS"
+	kubectl get deployment 
+	kubectl delete deployment 
+	kubectl delete pod <--all OR POD NAME> -- If POD is deployed using run command 
+	kubectl get pods -l run=my-nginx -o yaml
+	kubectl get pods -l run=my-nginx -o wide
+
+
+--> "NETWORKING"
+	etcdctl get /atomic.io/network/config
+
+
+
+--> "MISC"
+	kubectl -s x.x.x.x:8080 version -- to check version
+
+kubectl get pods -L run -- to check Labels
+kubectl get service/my-nginx
+
+
+--> create the kube-system namespace, the rc and service for kube-ui
+	"bin/kubectl -s x.x.x.x:8080 create -f kube-ui-rc.yaml --namespace=kube-system" 
+	"bin/kubectl -s x.x.x.x:8080 create -f -f kube-ui-svc.yaml --namespace=kube-system"
+
+	"bin/kubectl -s x.x.x.x:8080 get services --namespace=kube-system"
+	"bin/kubectl -s x.x.x.x:8080 get rc --namespace=kube-system"
+
+
+
+--> kubectl delete rc kubernetes-dashboard --namespace=kube-system
+
+--> "kubectl exec <pod-name> date" EXEC command against container in a POD
+
+1. kubectl get pods
+2. kubectl get services
+3. kubectl get ep 
+4. kubectl describe services my-nginx
+5. kubectl exec my-nginx-3800858182-46owm -- printenv | grep SERVICE
+6. kubectl resize rc <pod-name> --replicas=3
+
+
+to launch and expose to internet (https://www.youtube.com/watch?v=DC7NECq3Ghs) *This example is used to deploy=my-nginx with king DEPLOYMENT
+1. deploy POD (check the kind becuase rest of the commands depend if the pod is deploy as kind: POD, DEPLOYMENT or RC)
+2. k get/service pods
+3. check via curl to see if it working
+4. kubectl scale --current-replicas=1 --replicas=3 deployment/my-nginx (ADD containers for kind:deployment from 1 to 3)
+5. kubectl get pods(you should see 3 containers)
+6. check with curl 
+7. kubectl expose -f nginx-kindDeployment.yaml --port=80 --external-ip=192.168.245.250 (assign service/LB IP to aboce three container)
+		OR
+	kubectl expose -f nginx-kindDeployment.yaml --port=80 --type=NodePort
+			and Access the container on Node IP: Node Port
+8 kubectl get services
+9. curl with the service IP
+
+
+
+Training
+https://github.com/RedHatWorkshops/docker-kubernetes-workshop
+
+
+kubectl expose (-f FILENAME | TYPE NAME) [--port=port] [--protocol=TCP|UDP] [--target-port=number-or-name] [--name=name] [--external-ip=external-ip-of-service] [--type=type] [flags]
